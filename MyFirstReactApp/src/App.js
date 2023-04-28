9f320b8e31e99ef52a637d268c935a17858c5ef1
SwartzJonah <swartzjonah@gmail.com>
2023-04-28 16:19:17 -0400

    adding elements to dom

--- MyFirstReactApp/src/App.js
+++ MyFirstReactApp/src/App.js
@@ -2,10 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+
+  const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+  };
+
+  const element = (
+    <div>
+      <h1>Hello!</h1>
+      <h2>Good to see you here.</h2>
+    </div>
+  )
+
+  function getGreeting(user) {
+    if (user) {
+      return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
+  }
   return (
     <div className="App">
       <header className="App-header">
+        {getGreeting(user)}
+        {element}
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +40,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          What up playa
+          learn react
         </a>
       </header>
     </div>
