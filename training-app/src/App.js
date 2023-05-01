892def495b533831999fa1ec21d6483fac76bef0
SwartzJonah <swartzjonah@gmail.com>
2023-05-01 17:37:48 -0400

    state of App, task and task array, and changeName function and onSubmit function

--- training-app/src/App.js
+++ training-app/src/App.js
@@ -3,19 +3,45 @@ import React, { Component } from "react";
 import Overview from './components/Overview';
 
 class App extends Component {
-  constructor(props){
-    super(props);
+  constructor(){
+    super();
 
-    this.onSubmitBtn = this.onSubmitBtn.bind(this);
+    this.state = {
+      task: { name: ''},
+      taskArray: [],
+    };
   }
 
-  onSubmitBtn(){
-      console.log("submitted")
+  changeName(event){
+    this.setState({
+      task : { name:event.target.value }
+    })
   }
+
+  onSubmit(event){
+    event.preventDefault();
+    this.setState({
+      taskArray: this.state.taskArray.push(this.state.task),
+      task: {name: ''},
+    })
+  }
+
 
 
   render() {
-    return <div> <Overview title ="test name" /> </div>;
+    return (
+    <div>
+      <form>
+        <label htmlFor="taskName">Name of task? </label>
+        <input type="text" id="taskName"/>
+        <button type="submit">
+          Add task
+        </button>
+      </form>
+
+    </div>
+
+    );
   }
 }
 
