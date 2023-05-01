5e50c07c71dc5ea4998b90acef2d3e1db5d138fc
SwartzJonah <swartzjonah@gmail.com>
2023-05-01 17:02:14 -0400

    state practice

--- MySecondReactApp/second-app/src/MyComponent.js
+++ MySecondReactApp/second-app/src/MyComponent.js
@@ -1,20 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class MyComponent extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { title, onButtonClicked } = this.props;
-
-    return (
-      <div>
-        <h1>{title}</h1>
-        <button onClick={onButtonClicked}>Click Me!</button>
-      </div>
-    );
-  }
+function MyComponent({title}) {
+  return <div>{title}</div>;
 }
 
 export default MyComponent;
